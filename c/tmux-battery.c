71abbf76c0a07c3a8263e91147a31e267391c903
Josh Oldenburg <joshthegeek@me.com>
2014-08-15 22:09:22 -0400

    Clean up the C version.

--- c/tmux-battery.c
+++ c/tmux-battery.c
@@ -3,47 +3,63 @@
 #include <IOKit/ps/IOPowerSources.h>
 #include <IOKit/ps/IOPSKeys.h>
 
-SInt64 JOGetSInt64(CFNumberRef);
-
 static const int threshold = 10;
 
+struct batteryinfo {
+	Boolean isPresent;
+	Boolean externalConnected;
+	Boolean fullyCharged;
+	Boolean level;
+};
+struct batteryinfo *JOGetBatteryInfo(void);
+
 int main() {
+	struct batteryinfo *obj = JOGetBatteryInfo();
+
+	if (!obj->isPresent) {
+		printf("no battery\n");
+		return 1;
+	}
+
+	if (obj->externalConnected) {
+		if (obj->fullyCharged) printf("✔");
+		else printf("▲ %d%%", obj->level);
+	} else {
+		if (obj->level < threshold) printf("▼ %d%%!", obj->level);
+		else printf("▼ %d%%", obj->level);
+	}
+
+	printf("\n");
+	free(obj);
+	return 0;
+}
+
+Boolean JOGetBooleanForKey(CFDictionaryRef, CFStringRef);
+SInt64 JOGetSInt64(CFNumberRef);
+
+struct batteryinfo *JOGetBatteryInfo() {
 	CFTypeRef blob = IOPSCopyPowerSourcesInfo();
 	CFArrayRef arr = IOPSCopyPowerSourcesList(blob);
 	CFDictionaryRef obj = IOPSGetPowerSourceDescription(blob, CFArrayGetValueAtIndex(arr, 0));
 
 	if (!obj) {
 		printf("no battery\n");
-		return 1;
+		exit(1);
 	}
 
-	//CFShow(blob);
-	//CFShow(arr);
-	//CFShow(obj);
+	struct batteryinfo *info = malloc(sizeof(struct batteryinfo));
 
-	CFBooleanRef bIsPresent = CFDictionaryGetValue(obj, CFSTR(kIOPSIsPresentKey));
-	CFBooleanRef bIsCharged = CFDictionaryGetValue(obj, CFSTR(kIOPSIsChargedKey));
+	info->externalConnected = (CFStringCompare(IOPSGetProvidingPowerSourceType(blob), CFSTR(kIOPSACPowerValue), 0) == kCFCompareEqualTo);
+	info->isPresent = JOGetBooleanForKey(obj, CFSTR(kIOPSIsPresentKey));
+	info->fullyCharged = JOGetBooleanForKey(obj, CFSTR(kIOPSIsChargedKey));
+	info->level = (JOGetSInt64(CFDictionaryGetValue(obj, CFSTR(kIOPSCurrentCapacityKey))) * 100 / JOGetSInt64(CFDictionaryGetValue(obj, CFSTR(kIOPSMaxCapacityKey))));
 
-	Boolean isPresent = bIsPresent ? CFBooleanGetValue(bIsPresent) : false;
-	Boolean externalConnected = (CFStringCompare(IOPSGetProvidingPowerSourceType(blob), CFSTR(kIOPSACPowerValue), 0) == kCFCompareEqualTo);
-	Boolean fullyCharged = bIsCharged ? CFBooleanGetValue(bIsCharged) : false;
-	int level = (JOGetSInt64(CFDictionaryGetValue(obj, CFSTR(kIOPSCurrentCapacityKey))) * 100 / JOGetSInt64(CFDictionaryGetValue(obj, CFSTR(kIOPSMaxCapacityKey))));
+	return info;
+}
 
-	if (!isPresent) {
-		printf("no battery\n");
-		return 1;
-	}
-
-	if (externalConnected) {
-		if (fullyCharged) printf("✔");
-		else printf("▲ %d%%", level);
-	} else {
-		if (level < threshold) printf("▼ %d%%!", level);
-		else printf("▼ %d%%", level);
-	}
-
-	printf("\n");
-	return 0;
+Boolean JOGetBooleanForKey(CFDictionaryRef obj, CFStringRef key) {
+	CFBooleanRef bValue = CFDictionaryGetValue(obj, key);
+	return bValue ? CFBooleanGetValue(bValue) : false;
 }
 
 SInt64 JOGetSInt64(CFNumberRef number) {
